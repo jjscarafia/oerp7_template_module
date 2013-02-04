e041fe09d8b7d6e586dbee1b025ce9a95d65c92f
Juan José Scarafía <scarafia.juanjose@gmail.com>
2013-02-04 19:11:40 -0200

    Tempalte added

---
+++ template_module/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    
+#    Copyright (C) 2013 No author.
+#    No email
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{   'active': False,
+    'author': 'No author.',
+    'category': 'base.module_category_hidden',
+    'demo_xml': [],
+    'depends': ['base'],
+    'description': 'Descripción del Modulo template de ejemplo',
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'Modulo template de ejemplo',
+    'test': [],
+    'update_xml': [   u'security/ejemplo_group.xml',
+                      u'view/ejemplo_view.xml',
+                      u'view/ejemplo_menuitem.xml', -->
+                      u'workflow/ejemplo_workflow.xml',
+                      'security/ir.model.access.csv'],
+    'version': 'No version',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
